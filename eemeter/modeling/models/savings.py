b946e9af9c8605e593d0a22f48653b04a6953d5d
toshi09 <toshi.09vs@gmail.com>
2017-10-27 04:58:32 +0000

    Simple Savings Model

--- eemeter/modeling/models/savings.py
+++ eemeter/modeling/models/savings.py
@@ -1,53 +1,120 @@
 import pandas as pd
-from scipy.stats.stats import pearsonr
-from scipy import stats
 import statsmodels.formula.api as smf
 import numpy as np
+from sklearn.metrics import mean_squared_error
+from math import sqrt
 
-gas_file = "/vagrant/scripts/gas.csv"
+GAS_ENERGY = 'NATURAL_GAS_CONSUMPTION_SUPPLIED'
+ELECTREICITY_ENERGY = 'ELECTRICITY_CONSUMPTION_SUPPLIED'
 
-def get_data(fname, energy_type):
-    data = pd.read_csv(fname)
+def read_savings_data(fname, energy_type):
+    df = pd.read_csv(fname)
     if energy_type == 'gas':
-        data = data[['heating_coefficient',
-                     'intercept_coefficient',
-                     'natural_gas_savings_thm']]
+        df = df[['heating_coefficient',
+                 'intercept_coefficient',
+                 'project_climate_zone',
+                 'natural_gas_savings_thm']]
     else:
-        data = data[['heating_coefficient',
-                     'cooling_coefficient',
-                     'intercept_coefficient',
-                     'electricity_savings_kwh']]
-    return data
-
-def prediction(fitted_model_ols, dataframe):
-    return fitted_model_ols.predict(dataframe)
+        df = df[['heating_coefficient',
+                 'cooling_coefficient',
+                 'intercept_coefficient',
+                 'project_climate_zone',
+                 'electricity_savings_kwh']]
+    return df
 
 
-def rmse_train(fitted_model_ols):
-    return np.sqrt(fitted_model_ols.ssr/fitted_model_ols.nobs)
+def train_test_split(df, train_frac=0.8):
+    mask = np.random.rand(len(df)) < train_frac
+    train = df[mask]
+    test = df[~mask]
+    return train, test
 
-def fit(dataframe,
-        formula):
-    ols_model = smf.ols(formula=formula, data=dataframe)
-    fitted_model = ols_model.fit()
-    return ols_model, fitted_model
 
-def correlation(dataframe, energy_type):
-    if energy_type == 'gas':
-        dataframe = dataframe[dataframe.heating_coefficient.notnull()]
-    if energy_type == 'elec':
-        dataframe = dataframe[dataframe.cooling_coefficient.notnull()]
-        dataframe = dataframe[dataframe.heating_coefficient.notnull()]
+class SimpeSavingPredictionUsingHistoricalData:
+    """
+    A simple linear regression based model to predict savings for potential
+    clients.
+    We build a simple linear regression model from our past savings data which
+    has following information for each traces
+    1. heating_coefficient, 2. cooling_coefficient, 3. intercept_coef
+    4. annualized savings in Kwh
+    The goal is to learn weights of coeficients in predicting savings.
+    For new clients, we will build our usual Caltrack based models to learn
+    coeficients. We can then use the coeficient and weights learned from this
+    model to predict annualized savings for client..
+    """
+    def __init__(self, energy_type,
+                 include_climate_zone=False,
+                 response_var_name=None,
+                 model_weights=None,
+                 formula=None):
 
-    if energy_type == 'gas':
-        return pearsonr(dataframe['natural_gas_savings_th'],
-                        dataframe['heating_coefficient'])
+        self.model_weights = model_weights
+        self.energy_type = energy_type
+        self.response_var_name = response_var_name
+        if formula:
+            self.formula = formula
+        elif energy_type == GAS_ENERGY:
+            self.formula = "natural_gas_savings_thm ~ heating_coefficient + intercept_coefficient"
+            self.response_var_name = 'natural_gas_savings_thm'
+        elif energy_type == ELECTREICITY_ENERGY:
+            self.formula = "electricity_savings_kwh ~ cooling_coefficient +\
+            heating_coefficient + intercept_coefficient"
+            self.response_var_name = 'electricity_savings_kwh'
+        else:
+            raise ValueError("")
+        if not formula and include_climate_zone:
+            self.formula = formula + " + project_climate_zone"
 
-    else:
-        return pearsonr(dataframe['electricity_savings_kwh'],
-                        dataframe['heating_coefficient'])
+        self.fitted_model = None
+        self.model_obj = None
 
-if __name__ == "__main__":
-    energy_type = 'gas'
-    df = get_data(gas_file)
-    df = df.dropna()
+    def __init__(self, model_params):
+        self.model_weights = model_params
+
+    def predict(self, df):
+        pred = self.fitted_model.predict(df)
+        # Series
+        return pred
+
+    def predict_with_model_weights(self, feature_values):
+        if not self.model_weights:
+            raise ValueError("Model Weights Not Set")
+
+        if 'Intercept' not in self.model_weights:
+            raise ValueError("Intercept not present in model weights")
+
+        model_weight_keys = self.model_weights.keys() - set("Intercept")
+        if model_weight_keys != feature_values.keys():
+            raise ValueError("Feature Keys and Model Weights Keys do not match")
+
+        saving = self.model_weights['Intercept']
+        for feature_key, value in feature_values.items():
+            feature_weight = self.model_weights[feature_key]
+            saving += (value * feature_weight)
+
+        return saving
+
+    def out_of_sample_stats(self, df, response_var_name=None):
+        prediction = self.predict(df)
+        if response_var_name is None:
+            reponse_var_name = self.response_var_name
+        actual = df[reponse_var_name]
+        rmse = sqrt(mean_squared_error(prediction, actual))
+
+        savings = 0.0
+        for idx, predicted_saving in enumerate(prediction):
+            if predicted_saving > 0.0 and actual.iloc[idx] > 0.0:
+                savings += 1.0
+
+        savings_precision = savings / len(df)
+        print (savings, ' ', len(df))
+        return { 'rmse': rmse,
+                 'savings_precision' : savings_precision }
+
+    def fit(self, data_frame):
+        ols_model = smf.ols(formula=self.formula, data=data_frame)
+        self.fitted_model = ols_model.fit()
+        self.model_obj = ols_model
+        self.model_weights =  self.fitted_model.params.to_dict()
+        return self.model_weights
